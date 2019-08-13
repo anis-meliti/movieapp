87b18edeab6854620c8cdc3a3dee2830f8ad9fa2
Anis Melliti <ansmeliti@gmail.com>
2019-08-13 22:05:39 +0100

    made the architecture of the project

---
+++ src/Components/Movies/Movie/MovieCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MovieCard extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MovieCard
---
+++ src/Components/Movies/Movies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Movies extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Movies
