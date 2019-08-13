67a090dbb8294cb44a30966351c6c28981b2fb46
Anis Melliti <ansmeliti@gmail.com>
2019-08-13 22:41:07 +0100

    create the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Myheader from './Components/Myheader/Myheader';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Myheader></Myheader>
+    </div>
   );
 }
 
--- src/Components/Myheader/Myheader.js
+++ src/Components/Myheader/Myheader.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+// ******Style*********
+import './Myheader.css'
+import { Container, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap'
 
 class Myheader extends Component {
     render() {
         return (
-            <div>
+            <Container>
+                <Row>
+                    <Col md={8}>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="Search movie by name...."
 
-            </div>
+                            />
+                            <InputGroup.Append>
+                                <Button variant="outline-secondary" className="searchbtn"></Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+
+                    </Col>
+                    <Col>
+
+                    </Col>
+
+                </Row>
+            </Container>
         )
     }
 }
