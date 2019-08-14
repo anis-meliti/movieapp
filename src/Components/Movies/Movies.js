6b2caee00ef1581df9d94f4d20bee0db4bfe633b
Anis Melliti <ansmeliti@gmail.com>
2019-08-14 14:00:37 +0100

    fix the allignement of components

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import MovieList from '../assets/json/Movies.json'
 import MovieCard from './Movie/MovieCard.js';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 class Movies extends Component {
     render() {
         return (
-            <Col>
-                {MovieList.map((el, i) => <MovieCard title={el.title} poster={el.poster} rating={el.rating} key={i} />)}
-            </Col>
+            <Container>
+                <Row>
+                    {MovieList.map((el, i) => <MovieCard title={el.title} poster={el.poster} rating={el.rating} key={i} />)}
+
+                </Row>
+            </Container>
         )
     }
 }
